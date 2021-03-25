87251391f98190c21c4c54b3a64e90a9adc6f177
infopirate <aseriousgogetta@gmail.com>
2021-03-24 19:41:27 -0500

    Update check.ts

--- pages/api/check.ts
+++ pages/api/check.ts
@@ -7,13 +7,13 @@ export default async (req, res) => {
   const { userAddress, tokenAddress, tokenId, network } = req.query;
 
   const networks = {
-    1: 'wss://main-light.eth.linkpool.io/ws',
+    2: 'wss://node.expanse.tech/ws',
     4: 'wss://rinkeby-light.eth.linkpool.io/ws',
     137: 'wss://ws-mainnet.matic.network',
     80001: 'wss://ws-mumbai.matic.today',
   };
 
-  const web3 = new Web3(networks[network || 80001]);
+  const web3 = new Web3(networks[network || 2]);
 
   const tokenContract = new web3.eth.Contract(ERC721ABI, tokenAddress);
 
